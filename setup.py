396c5ba40662bc71c075fc41e9b30950f095e775
Windfarer <windfarer@gmail.com>
2018-04-17 09:59:00 +0800

    version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Windfarer/biu'
 EMAIL = 'windfarer@gmail.com'
 AUTHOR = 'Windfarer'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.2'
+VERSION = '0.1.0'
 
 REQUIRED = [
     'gevent', 'requests', 'parsel',
